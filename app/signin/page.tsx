42e8dd49e91fd12ac41d7c81bc262ee5ad283b2b
Prathamesh Sanjay Lakare <prathamesh.sanjay@geekyants.com>
2025-07-14 20:07:21 +0530

    feat: if user login it should redirect home page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,14 @@
+import { getServerSession } from "next-auth";
 import Signin from "../components/common/Signin";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function SigninPage() {
+export default async function SigninPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/");
+  }
   return (
     <>
       <Signin />
