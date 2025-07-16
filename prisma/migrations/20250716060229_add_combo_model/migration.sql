dd640e92f98dd15946f5ac8ba1d1c316d5fa3043
Prathamesh Sanjay Lakare <prathamesh.sanjay@geekyants.com>
2025-07-16 11:54:47 +0530

    feat: create model for combo and combo item

---
+++ prisma/migrations/20250716060229_add_combo_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Combo" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Combo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Combo" ADD CONSTRAINT "Combo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
