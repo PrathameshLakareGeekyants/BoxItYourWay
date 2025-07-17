242f357f40ccc57f193a28e54df1d898bf363ec2
Prathamesh Sanjay Lakare <prathamesh.sanjay@geekyants.com>
2025-07-17 17:56:07 +0530

    delete: remove query from prisma client

--- lib/prisma/index.ts
+++ lib/prisma/index.ts
@@ -9,7 +9,7 @@ const globalForPrisma = globalThis as unknown as {
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"], // optional: helpful in dev
+    log: [],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
