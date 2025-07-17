240f2e9389b283ee7d54f37d1a674c23da6a0af5
Prathamesh Sanjay Lakare <prathamesh.sanjay@geekyants.com>
2025-07-17 13:25:01 +0530

    feat: add trycatch block and also add athentication

--- app/api/combo/route.ts
+++ app/api/combo/route.ts
@@ -3,73 +3,86 @@ import { getAuthSession } from "@/lib/service/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const session = await getAuthSession();
+  try {
+    const session = await getAuthSession();
 
-  // if (!session) {
-  //   return NextResponse.json(
-  //     { error: "You must be signed in to create a product." },
-  //     { status: 401 }
-  //   );
-  // }
+    if (!session) {
+      return NextResponse.json(
+        { error: "You must be signed in to create a product." },
+        { status: 401 }
+      );
+    }
 
-  const userId = session?.user?.id;
+    const userId = session?.user?.id;
 
-  const usersCombos = userId
-    ? await prisma.combo.findMany({
-        where: { userId },
-        include: {
-          comboItem: {
-            include: { product: true },
+    const usersCombos = userId
+      ? await prisma.combo.findMany({
+          where: { userId },
+          include: {
+            comboItem: {
+              include: { product: true },
+            },
           },
+        })
+      : [];
+
+    const publicCombos = await prisma.combo.findMany({
+      where: { userId: null },
+      include: {
+        comboItem: {
+          include: { product: true },
         },
-      })
-    : [];
-
-  const publicCombos = await prisma.combo.findMany({
-    where: { userId: null },
-    include: {
-      comboItem: {
-        include: { product: true },
       },
-    },
-  });
+    });
 
-  return NextResponse.json({
-    message: "Combos fetched successfully..",
-    usersCombos,
-    publicCombos,
-  });
+    return NextResponse.json({
+      message: "Combos fetched successfully..",
+      usersCombos,
+      publicCombos,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
 }
 
 export async function POST(req: Request) {
-  const session = await getAuthSession();
+  try {
+    const session = await getAuthSession();
 
-  // if (!session) {
-  //   return NextResponse.json(
-  //     { error: "You must be signed in to create a product." },
-  //     { status: 401 }
-  //   );
-  // }
+    if (!session) {
+      return NextResponse.json(
+        { error: "You must be signed in to create a product." },
+        { status: 401 }
+      );
+    }
 
-  const userId = session?.user?.id;
+    const userId = session?.user?.id;
 
-  const body = await req.json();
-  const { name } = body;
+    const body = await req.json();
+    const { name, productIds } = body;
 
-  if (!name || !userId) {
-    return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+    if (!name || !userId) {
+      return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+    }
+
+    const newCombo = prisma.combo.create({
+      data: {
+        name,
+        userId: session.user.id,
+        comboItem: {
+          create: productIds.map((productId: string) => ({
+            product: { connect: { id: productId } },
+          })),
+        },
+      },
+    });
+
+    return NextResponse.json({
+      message: "New combo created successfully.",
+      newCombo,
+      status: 201,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
-
-  const newCombo = prisma.combo.create({
-    data: {
-      name,
-      userId,
-    },
-  });
-
-  return NextResponse.json({
-    message: "New combo created successfully.",
-    newCombo,
-    status: 201,
-  });
 }
