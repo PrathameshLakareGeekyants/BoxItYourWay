19334a0cd47acf68a40904cae1a60f84c4f0a85f
Prathamesh Sanjay Lakare <prathamesh.sanjay@geekyants.com>
2025-07-29 11:30:43 +0530

    feat: add API functions for get and create delivery address

---
+++ lib/service/delivery.ts
@@ -0,0 +1,20 @@
+import { DELIVERY_API } from "@/constants";
+import { fetchData } from "../network/fetcher";
+import { postData } from "../network/post";
+
+export async function fetchDeliveryInfos() {
+  return fetchData(DELIVERY_API);
+}
+
+export async function createDeliveryInfo(data: {
+  addressLine: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  country: string;
+  contact: string;
+}) {
+  const res = postData(DELIVERY_API, data);
+
+  return res;
+}
