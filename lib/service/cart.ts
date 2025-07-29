ffb6cc0c18ff32fe4754a2d45fff5c897bd114f5
Prathamesh Sanjay Lakare <prathamesh.sanjay@geekyants.com>
2025-07-29 11:27:57 +0530

    eat: add API functions for updating and deleting cart items

--- lib/service/cart.ts
+++ lib/service/cart.ts
@@ -1,6 +1,7 @@
 import { CART_API } from "@/constants";
 import { fetchData } from "../network/fetcher";
 import { postData } from "../network/post";
+import { deleteData } from "../network/delete";
 
 export async function getCartItems() {
   return fetchData(CART_API);
@@ -13,9 +14,45 @@ export async function addToCart({
   productId?: string | undefined;
   comboId?: string | undefined;
 }) {
-  console.log("comboID", comboId);
-  console.log("productID", productId);
+  if (productId) {
+    return postData(CART_API, { productId });
+  } else if (comboId) {
+    return postData(CART_API, { comboId });
+  }
+}
 
-  const data = "";
-  return postData(CART_API, data);
+//cart item api's
+export async function updateQuantity({
+  cartId,
+  comboId,
+  productId,
+  delta,
+}: {
+  cartId: string;
+  comboId?: string;
+  productId?: string;
+  delta: number;
+}) {
+  if (productId) {
+    return postData(`${CART_API}/update-quantity`, {
+      cartId,
+      productId,
+      delta,
+    });
+  } else if (comboId) {
+    return postData(`${CART_API}/update-quantity`, { cartId, comboId, delta });
+  }
+}
+
+export async function deleteCartItem({
+  cartId,
+  cartItemId,
+}: {
+  cartId: string;
+  cartItemId: string;
+}) {
+  return deleteData(`${CART_API}/cart-item`, {
+    cartId,
+    cartItemId,
+  });
 }
