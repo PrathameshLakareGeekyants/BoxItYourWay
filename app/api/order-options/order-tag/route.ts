c15e0225f66604b688a31fd77355643a85722503
Prathamesh Sanjay Lakare <prathamesh.sanjay@geekyants.com>
2025-08-05 16:24:32 +0530

    feat: update to set price and also get both public and custom tags

--- app/api/order-options/order-tag/route.ts
+++ app/api/order-options/order-tag/route.ts
@@ -12,10 +12,15 @@ export async function GET() {
       );
     }
 
-    const orderTags = await prisma.orderTag.findMany();
-    return NextResponse.json({ orderTags }, { status: 200 });
+    const myOrderTags = await prisma.orderTag.findMany({
+      where: { userId: session.user.id, isPublic: false },
+    });
+    const publicTags = await prisma.orderTag.findMany({
+      where: { isPublic: true },
+    });
+    return NextResponse.json({ myOrderTags, publicTags }, { status: 200 });
   } catch (error) {
-    console.error("error for wrap route: ", error);
+    console.error("error for order tag route: ", error);
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
@@ -32,10 +37,9 @@ export async function POST(req: Request) {
 
     const userId = session.user.id;
     const body = await req.json();
+    const { name, isPublic, price } = body;
 
-    if (session?.user?.role !== "ADMIN") {
-      const { name, isPublic } = body;
-
+    if (session.user.role !== "ADMIN") {
       if (isPublic === true) {
         return NextResponse.json(
           { error: "You must be an admin to set isPublic true" },
@@ -43,22 +47,20 @@ export async function POST(req: Request) {
         );
       }
 
+      const finalPrice = typeof price === "number" ? price : 10;
       const orderTag = await prisma.orderTag.create({
-        data: { name, userId, isPublic },
+        data: { name, userId, isPublic, price: finalPrice },
       });
-
       return NextResponse.json({ orderTag }, { status: 201 });
     }
 
-    const { name } = body;
-
     const orderTag = await prisma.orderTag.create({
-      data: { name, userId },
+      data: { name, userId, isPublic, price: 10 },
     });
 
     return NextResponse.json({ orderTag }, { status: 201 });
   } catch (error) {
-    console.error("error for wrap route: ", error);
+    console.error("error for order tag route: ", error);
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
